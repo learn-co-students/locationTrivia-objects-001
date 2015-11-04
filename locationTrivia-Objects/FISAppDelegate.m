935469e231193c6ba5bb3ae2b3091933228627b1
yongbingao <ben.gao25@gmail.com>
2015-11-03 22:02:58 -0500

    Location Trivia Objects Lab done

--- locationTrivia-Objects/FISAppDelegate.m
+++ locationTrivia-Objects/FISAppDelegate.m
@@ -30,24 +30,25 @@
 }
 
 - (NSArray *)locationsNearLatitude:(CGFloat)latitude longitude:(CGFloat)longitude margin:(CGFloat)margin {
-    NSMutableArray *allLocationsWithin = [[NSMutableArray alloc]init];
     CGFloat latPlus = latitude + margin;
     CGFloat latMinus = latitude - margin;
     CGFloat longPlus = longitude + margin;
     CGFloat longMinus = longitude - margin;
-    for (FISLocation *locationsInFISLocation in self.locations) {
-        if (locationsInFISLocation.latitude >= latMinus &&
-            locationsInFISLocation.latitude <= latPlus &&
-            locationsInFISLocation.longitude >= longMinus &&
-            locationsInFISLocation.longitude <= longPlus) {
-            [allLocationsWithin addObject:locationsInFISLocation];
-        }
-    }
-/*    NSPredicate *latitudeMargin = [NSPredicate predicateWithFormat:@"latitude <= %f AND latitude >= %f AND longitude <= %f AND longitude >= %f ", latPlus, latMinus, longPlus, longMinus];
+
+//   NSMutableArray *allLocationsWithin = [[NSMutableArray alloc]init];
+//   for (FISLocation *locationsInFISLocation in self.locations) {
+//        if (locationsInFISLocation.latitude >= latMinus &&
+//            locationsInFISLocation.latitude <= latPlus &&
+//            locationsInFISLocation.longitude >= longMinus &&
+//            locationsInFISLocation.longitude <= longPlus) {
+//            [allLocationsWithin addObject:locationsInFISLocation];
+//        }
+//    }
+    NSPredicate *latitudeMargin = [NSPredicate predicateWithFormat:@"latitude <= %lf AND latitude >= %lf AND longitude <= %lf AND longitude >= %lf ", latPlus , latMinus, longPlus, longMinus];
     NSArray *array = [self.locations filteredArrayUsingPredicate:latitudeMargin];
     NSLog(@"%f/n%f/n%f/n%f", latPlus, latMinus, longPlus, longMinus);
- */
-    return allLocationsWithin;
+
+    return array;
 }
 
 @end
