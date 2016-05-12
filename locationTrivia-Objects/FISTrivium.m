460328950264661ee340db558d59c99aa87f49c4
John Richardson <richardsonjm@gmail.com>
2016-05-12 10:48:25 -0400

    Build FISTrivium class

--- locationTrivia-Objects/FISTrivium.m
+++ locationTrivia-Objects/FISTrivium.m
@@ -11,13 +11,18 @@
 @implementation FISTrivium
 
 - (instancetype)init {
-    self = [super init];
+    self = [self initWithContent:@""
+                           likes:0];
     return self;
 }
 
 - (instancetype)initWithContent:(NSString *)content
                           likes:(NSUInteger)likes {
     self = [super init];
+    if (self) {
+        _content = content;
+        _likes = likes;
+            }
     return self;
 }
 
