b7006acf4ec6f9912d9183a61e9d512ecd90fb8f
John Richardson <richardsonjm@gmail.com>
2016-05-12 11:13:46 -0400

    Build location class

--- locationTrivia-Objects/FISLocation.m
+++ locationTrivia-Objects/FISLocation.m
@@ -11,7 +11,10 @@
 @implementation FISLocation
 
 - (instancetype)init {
-    self = [super init];
+    self = [self initWithName:@""
+                     latitude:0
+                    longitude:0];
+    self.trivia = [[NSMutableArray alloc] init];
     return self;
 }
 
@@ -19,19 +22,47 @@
                     latitude:(CGFloat)latitude
                    longitude:(CGFloat)longitude {
     self = [super init];
+    if (self) {
+        _name = name;
+        _latitude = latitude;
+        _longitude = longitude;
+    }
+    self.trivia = [[NSMutableArray alloc] init];
     return self;
 }
 
 - (NSString *)stringByTruncatingNameToLength:(NSUInteger)length {
-    return nil;
+    if (length <= [self.name length]) {
+        return [self.name substringToIndex:length];
+    } else {
+        return self.name;
+    }
 }
 
 - (BOOL)hasValidData {
-    return NO;
+    if ([self.name length] == 0) {
+        return NO;
+    } else if (ABS(self.latitude) > 90.0) {
+        return NO;
+    } else if (ABS(self.longitude) > 180.0 || self.longitude == -180.0) {
+        return NO;
+    } else {
+        return YES;
+    }
 }
 
 - (FISTrivium *)triviumWithMostLikes {
-    return nil;
+    if ([self.trivia count] == 0) {
+        return nil;
+    } else {
+        FISTrivium *mostLikedTrivium = [self.trivia objectAtIndex:0];
+        for (NSUInteger i = 1; i < [self.trivia count]; i++) {
+            if ([self.trivia[i] likes] > [mostLikedTrivium likes] ) {
+                mostLikedTrivium = self.trivia[i];
+            }
+        }
+        return mostLikedTrivium;
+    }
 }
 
 @end
